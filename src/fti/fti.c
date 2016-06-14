9cce456a7db6f6fba22e67b45c9843c7b98e54bb
Gaalich Mohamed <mohamed.gaalich@cea.fr>
2016-06-14 11:52:36 +0200

    new fti module

---
+++ src/fti/fti.c
@@ -0,0 +1,84 @@
+/*******************************************************************************
+ * Copyright (c) 2015, Julien Bigot - CEA (julien.bigot@cea.fr)
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ * * Neither the name of CEA nor the names of its contributors may be used to
+ *   endorse or promote products derived from this software without specific 
+ *   prior written permission.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ ******************************************************************************/
+
+#include <mpi.h>
+
+#include <pdi.h>
+#include <pdi/plugin.h>
+#include <pdi/state.h>
+#include <fti.h>
+
+MPI_Comm my_world;
+
+PDI_status_t PDI_fti_init(PC_tree_t conf, MPI_Comm *world)
+{
+	conf = conf; // prevent unused warning
+	my_world = *world;
+	
+	int rank; if (MPI_Comm_rank(my_world, &rank)) return PDI_ERR_PLUGIN;
+	
+	return PDI_OK;
+}
+
+PDI_status_t PDI_fti_finalize()
+{
+	int rank; if (MPI_Comm_rank(my_world, &rank)) return PDI_ERR_PLUGIN;
+	
+	return PDI_OK;
+}
+
+PDI_status_t PDI_fti_event(const char *event)
+{
+	int rank; if (MPI_Comm_rank(my_world, &rank)) return PDI_ERR_PLUGIN;
+	
+	if ( rank == 0 ) {
+		printf("test plugin got an event: %s!\n", event);
+	}
+	
+	return PDI_OK;
+}
+
+PDI_status_t PDI_fti_data_start(PDI_variable_t *data)
+{
+	int rank; if (MPI_Comm_rank(my_world, &rank)) return PDI_ERR_PLUGIN;
+	
+	if ( rank == 0 ) {
+		printf(">> data becoming available to the test plugin: %s!\n", data->name);
+	}
+	
+	return PDI_OK;
+}
+
+PDI_status_t PDI_fti_data_end(PDI_variable_t *data)
+{
+	int rank; if (MPI_Comm_rank(my_world, &rank)) return PDI_ERR_PLUGIN;
+	
+	if ( rank == 0 ) {
+		printf("<< data becoming unavailable to the test plugin: %s!\n", data->name);
+	}
+	
+	return PDI_OK;
+}
+
+PDI_PLUGIN(fti)
