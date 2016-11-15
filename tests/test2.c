0bbf10d06a7a9eeb15a0dad6e79e95dcc1b20ed5
Corentin Roussel <corentin.roussel@gmail.com>
2016-11-15 17:34:22 +0100

    Adding a test

---
+++ tests/test2.c
@@ -0,0 +1,54 @@
+/*******************************************************************************
+ * Copyright (c) 2015, Julien Bigot - CEA (julien.bigot@cea.fr)
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ * * Neither the name of CEA nor the names of its contributors may be used to
+ *   endorse or promote products derived from this software without specific 
+ *   prior written permission.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ ******************************************************************************/
+
+#include <assert.h>
+#include <unistd.h>
+#include <mpi.h>
+#include <pdi.h>
+
+int main( int argc, char *argv[] )
+{
+	int value=5;
+	double test_var=0;
+	MPI_Init(&argc, &argv);
+	assert(argc == 2 && "Needs 1 single arg: config file");
+	PC_tree_t conf = PC_parse_path(argv[1]);
+	MPI_Comm world = MPI_COMM_WORLD;
+	PDI_status_t err = PDI_init(conf, &world);
+
+	PDI_transaction_begin("testing");
+	PDI_expose("meta1",&value);
+	PDI_expose("test_var",&test_var);
+	PDI_transaction_end();
+	PDI_finalize();
+
+	char fname[] = "5.h5"
+	if (access( fname, F_OK ) == -1 ) {
+		printf("File not found.");
+		return -1;
+	}// file doesn't exist
+	PC_tree_destroy(&conf);
+	MPI_Finalize();
+	return 0;
+}
