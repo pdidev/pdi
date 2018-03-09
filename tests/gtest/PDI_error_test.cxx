0beb1bef4e94d0f4f8f6cc19d8195b0772800701
Karol Sierociński <ksiero@man.poznan.pl>
2018-03-09 14:41:34 +0100

    Added gtest, testing on Jenkins

---
+++ tests/gtest/PDI_error_test.cxx
@@ -0,0 +1,12 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include <pdi.h>
+#include <pdi/status.h>
+
+namespace {
+    TEST(statusTest, what) {
+        PDI::Error error(PDI_OK, "This is some error.");
+        ASSERT_STREQ("This is some error .", error.what());
+    }
+}
