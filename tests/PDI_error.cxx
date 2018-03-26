0d72104b3c7ff66b3d720dc89b233eff87476317
Julien Bigot <julien.bigot@cea.fr>
2018-03-26 15:14:31 +0200

    Added an indentation test

--- tests/PDI_error.cxx
+++ tests/PDI_error.cxx
@@ -29,38 +29,38 @@
 
 /*
  * Name:                ErrorTest.call_constructor_no_vargs
- * 
+ *
  * Tested functions:    PDI::Error::Error()
  *                      PDI::Error::status()
  *                      PDI::Error::what()
- * 
+ *
  * Description:         Test calls PDI::Error constructor without vargs
  *                      and checks what() and status() values.
- * 
+ *
  */
 TEST(ErrorTest, call_constructor_no_vargs)
 {
-    PDI::Error error(PDI_OK, "No error.");
-    ASSERT_STREQ("No error.", error.what());
-    ASSERT_EQ(error.status(), PDI_OK);
+	PDI::Error error(PDI_OK, "No error.");
+	ASSERT_STREQ("No error.", error.what());
+	ASSERT_EQ(error.status(), PDI_OK);
 }
 
 /*
  * Name:                ErrorTest.call_constructor_vargs
- * 
+ *
  * Tested functions:    PDI::Error::Error()
  *                      PDI::Error::status()
  *                      PDI::Error::what()
- * 
+ *
  * Description:         Test calls PDI::Error constructor with 2 vargs
  *                      and checks what() and status() values.
- * 
+ *
  */
 TEST(ErrorTest, call_constructor_vargs)
 {
-    PDI::Error error(PDI_UNAVAILABLE, "%d errors in %s?", 0,"ErrorTest");
-    ASSERT_STREQ("0 errors in ErrorTest?", error.what());
-    ASSERT_EQ(error.status(), PDI_UNAVAILABLE);
+	PDI::Error error(PDI_UNAVAILABLE, "%d errors in %s?", 0,"ErrorTest");
+	ASSERT_STREQ("0 errors in ErrorTest?", error.what());
+	ASSERT_EQ(error.status(), PDI_UNAVAILABLE);
 }
 
 /*
@@ -68,46 +68,46 @@ TEST(ErrorTest, call_constructor_vargs)
  */
 PDI::Error va_function(const char* msg, ...)
 {
-    va_list ap;
-    va_start(ap, msg);
-    PDI::Error error(PDI_OK, msg, ap);
-    va_end(ap);
-    return error;
+	va_list ap;
+	va_start(ap, msg);
+	PDI::Error error(PDI_OK, msg, ap);
+	va_end(ap);
+	return error;
 }
 
 /*
  * Name:                ErrorTest.call_constructor_va_list
- * 
+ *
  * Tested functions:    PDI::Error::Error()
  *                      PDI::Error::status()
  *                      PDI::Error::what()
- * 
- * Description:         Test calls PDI::Error constructor with va_list 
- *                      (containing 2 strings) and checks what() and 
+ *
+ * Description:         Test calls PDI::Error constructor with va_list
+ *                      (containing 2 strings) and checks what() and
  *                      status() values.
- * 
+ *
  */
 TEST(ErrorTest, call_constructor_va_list)
 {
-    PDI::Error error {va_function("Testing %s in %s", "what", "ErrorTest")};
-    ASSERT_STREQ("Testing what in ErrorTest", error.what());
-    ASSERT_EQ(error.status(), PDI_OK);
+	PDI::Error error {va_function("Testing %s in %s", "what", "ErrorTest")};
+	ASSERT_STREQ("Testing what in ErrorTest", error.what());
+	ASSERT_EQ(error.status(), PDI_OK);
 }
 
 /*
  * Name:                ErrorTest.call_constructor_empty_va_list
- * 
+ *
  * Tested functions:    PDI::Error::Error()
  *                      PDI::Error::status()
  *                      PDI::Error::what()
- * 
- * Description:         Test calls PDI::Error constructor with empty va_list 
+ *
+ * Description:         Test calls PDI::Error constructor with empty va_list
  *                      and checks what() and status() values.
- * 
+ *
  */
 TEST(ErrorTest, call_constructor_empty_va_list)
 {
-    PDI::Error error {va_function("This is some text.")};
-    ASSERT_STREQ("This is some text.", error.what());
-    ASSERT_EQ(error.status(), PDI_OK);
+	PDI::Error error {va_function("This is some text.")};
+	ASSERT_STREQ("This is some text.", error.what());
+	ASSERT_EQ(error.status(), PDI_OK);
 }
--- tests/PDI_member.cxx
+++ tests/PDI_member.cxx
@@ -16,90 +16,93 @@
 using namespace PDI;
 using ::testing::Return;
 
-struct MockDatatype : public Datatype 
-{
-    Datatype_template_uptr clone() const override {
-        return Datatype_template_uptr{clone_proxy()};
-    }
-
-    Datatype_uptr clone_type() const override {
-        return Datatype_uptr{clone_type_proxy()};
-    }
-
-    Datatype_uptr densify() const override {
-        return Datatype_uptr{densify_proxy()};
-    }
-
-    Datatype_uptr evaluate(Context&) const override {
-        return Datatype_uptr{evaluate_proxy()};
-    }
-
-    MOCK_CONST_METHOD0(dense, bool());
-    MOCK_CONST_METHOD0(datasize, size_t());
-    MOCK_CONST_METHOD0(buffersize, size_t());
-    MOCK_CONST_METHOD0(alignment, size_t());
-
-    MOCK_CONST_METHOD0(clone_type_proxy, MockDatatype*());
-    MOCK_CONST_METHOD0(clone_proxy, MockDatatype*());
-    MOCK_CONST_METHOD0(densify_proxy, MockDatatype*());
-    MOCK_CONST_METHOD0(evaluate_proxy, MockDatatype*());
+struct MockDatatype : public Datatype {
+	Datatype_template_uptr clone() const override
+	{
+		return Datatype_template_uptr{clone_proxy()};
+	}
+	
+	Datatype_uptr clone_type() const override
+	{
+		return Datatype_uptr{clone_type_proxy()};
+	}
+	
+	Datatype_uptr densify() const override
+	{
+		return Datatype_uptr{densify_proxy()};
+	}
+	
+	Datatype_uptr evaluate(Context&) const override
+	{
+		return Datatype_uptr{evaluate_proxy()};
+	}
+	
+	MOCK_CONST_METHOD0(dense, bool());
+	MOCK_CONST_METHOD0(datasize, size_t());
+	MOCK_CONST_METHOD0(buffersize, size_t());
+	MOCK_CONST_METHOD0(alignment, size_t());
+	
+	MOCK_CONST_METHOD0(clone_type_proxy, MockDatatype*());
+	MOCK_CONST_METHOD0(clone_proxy, MockDatatype*());
+	MOCK_CONST_METHOD0(densify_proxy, MockDatatype*());
+	MOCK_CONST_METHOD0(evaluate_proxy, MockDatatype*());
 };
 
 /*
  * Member is int with displacement = 8 and named "intMember"
  */
-struct ScalarMemberTest : public ::testing::Test
-{
-    ScalarMemberTest() {
-        test_displacement = 8;
-        test_name = "intMember";
-        test_member = std::unique_ptr<Record_datatype::Member> {new Record_datatype::Member(test_displacement, Datatype_uptr{mocked_datatype}, test_name)};
-    }
-
-    //displacement of member
-    size_t test_displacement;
-
-    MockDatatype* mocked_datatype {new MockDatatype()};
-
-    //name of the member
-    std::string test_name;
-
-    std::unique_ptr<Record_datatype::Member> test_member;
-    Record_datatype::Member* test_member_copy;
+struct ScalarMemberTest : public ::testing::Test {
+	ScalarMemberTest()
+	{
+		test_displacement = 8;
+		test_name = "intMember";
+		test_member = std::unique_ptr<Record_datatype::Member> {new Record_datatype::Member(test_displacement, Datatype_uptr{mocked_datatype}, test_name)};
+	}
+	
+	//displacement of member
+	size_t test_displacement;
+	
+	MockDatatype* mocked_datatype {new MockDatatype()};
+	
+	//name of the member
+	std::string test_name;
+	
+	std::unique_ptr<Record_datatype::Member> test_member;
+	Record_datatype::Member* test_member_copy;
 };
 
 /*
  * Name:                ScalarMemberTest.check_fields
- * 
+ *
  * Tested functions:    Record_datatype::Member::Member(size_t displacement, Datatype_uptr type, const string& name)
- * 
+ *
  * Description:         Test checks values of fields.
- * 
+ *
  */
 TEST_F(ScalarMemberTest, check_fields)
 {
-    ASSERT_EQ(8, test_member->displacement());
-    ASSERT_EQ(this->test_name, test_member->name());
-    ASSERT_EQ(this->mocked_datatype, &test_member->type());
+	ASSERT_EQ(8, test_member->displacement());
+	ASSERT_EQ(this->test_name, test_member->name());
+	ASSERT_EQ(this->mocked_datatype, &test_member->type());
 }
 
 
 /*
  * Name:                ScalarMemberTest.check_clone_constructor
- * 
+ *
  * Tested functions:    Record_datatype::Member::Member(const Member&)
- * 
+ *
  * Description:         Test checks clone constructor.
- * 
+ *
  */
 TEST_F(ScalarMemberTest, check_clone_constructor)
 {
-    MockDatatype* cloned_type {new MockDatatype()};
-    EXPECT_CALL(*mocked_datatype, clone_type_proxy())
-            .WillOnce(Return(cloned_type));
-    
-    Record_datatype::Member cloned {*test_member};
-    ASSERT_EQ(cloned.displacement(), test_member->displacement());
-    ASSERT_EQ(cloned.name(), test_member->name());
-    ASSERT_EQ(&cloned.type(), cloned_type);
+	MockDatatype* cloned_type {new MockDatatype()};
+	EXPECT_CALL(*mocked_datatype, clone_type_proxy())
+	.WillOnce(Return(cloned_type));
+	
+	Record_datatype::Member cloned {*test_member};
+	ASSERT_EQ(cloned.displacement(), test_member->displacement());
+	ASSERT_EQ(cloned.name(), test_member->name());
+	ASSERT_EQ(&cloned.type(), cloned_type);
 }
