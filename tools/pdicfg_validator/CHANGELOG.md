ab26d9699fe3314be96be2b17773972c95758962
Julien Bigot <julien.bigot@cea.fr>
2022-01-26 19:07:07 +0100

    Support the new API of recent PyYAML

--- tools/pdicfg_validator/CHANGELOG.md
+++ tools/pdicfg_validator/CHANGELOG.md
@@ -17,5 +17,7 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
 ### Fixed
 * Correctly handle systems where there is no python interpreter named `python`
   [#399](https://gitlab.maisondelasimulation.fr/pdidev/pdi/-/issues/399)
+* Support the new API of recent PyYAML
+  [#402](https://gitlab.maisondelasimulation.fr/pdidev/pdi/-/issues/402)
 
 ### Security
--- tools/pdicfg_validator/pdicfg_validator/__init__.py
+++ tools/pdicfg_validator/pdicfg_validator/__init__.py
@@ -26,6 +26,10 @@
 import importlib
 import os
 import yaml
+try:
+    from yaml import CLoader as Loader, CDumper as Dumper
+except ImportError:
+    from yaml import Loader, Dumper
 import sys
 import re
 
@@ -43,8 +47,9 @@ scalar_types = ['char',
                 'logical',
                 'real']
 
-# adds all references from string to data_refs list
+
 def add_to_data_ref(string, data_refs_list):
+    """ adds all references from string to data_refs list"""
     desc_name_re = re.compile(r'\$\{(\w+)(?:\}|\:\w+\})')
     if not desc_name_re.search(str(string)):
         desc_name_re = re.compile(r'\$(\w+)(?:\:\w+)?')
@@ -54,8 +59,9 @@ def add_to_data_ref(string, data_refs_list):
         return True
     return False
 
-# validate value of the size (numeric or reference)
+
 def val_size_value(value, data_refs_list):
+    """ validate value of the size (numeric or reference)"""
     if isinstance(value, int):
         pass
     elif isinstance(value, float):
@@ -64,16 +70,18 @@ def val_size_value(value, data_refs_list):
         if not add_to_data_ref(value, data_refs_list):
             raise NameError("`" + str(value) + "' is not valid value")
 
-# validate size property (scalar/reference or list of scalars/references)
+
 def val_size(size, data_refs_list):
+    """ validate size property (scalar/reference or list of scalars/references)"""
     if isinstance(size, list):
         for element in size:
             val_size_value(element, data_refs_list)
     else:
         val_size_value(size, data_refs_list)
 
-# validate array (check required properties and values)
+
 def val_array(value, data_refs_list):
+    """ validate array (check required properties and values)"""
     if 'size' not in value:
         raise NameError("Array must have `size' property: " + str(value))
     val_size(value['size'], data_refs_list)
@@ -85,15 +93,17 @@ def val_array(value, data_refs_list):
     if 'start' in value:
         val_size(value['start'], data_refs_list)
 
-# validate record member (must have disp and consist desc structure)
+
 def val_member(member, data_refs_list):
+    """validate record member (must have disp and consist desc structure)"""
     if 'disp' not in member:
         raise NameError("Member must have `disp' property: " + str(member))
     val_size(member['disp'], data_refs_list)
     val_desc(member, data_refs_list)
 
-# validate record (must have buffersize and members)
+
 def val_record(value, data_refs_list):
+    """ validate record (must have buffersize and members)"""
     if 'buffersize' not in value:
         raise NameError("Record must have `buffersize' property: " + str(value))
     val_size(value['buffersize'], data_refs_list)
@@ -102,23 +112,26 @@ def val_record(value, data_refs_list):
     for member in value['members'].values():
         val_member(member, data_refs_list)
 
-# validate struct (must have members)
+
 def val_struct(value, data_refs_list):
+    """validate struct (must have members)"""
     if 'members' not in value:
         raise NameError("Struct must have `members' property: " + str(value))
     for member_node in value['members']:
         for member_value in member_node.values():
             val_desc(member_value, data_refs_list)
 
-# validate tuple (must have elements)
+
 def val_tuple(value, data_refs_list):
+    """validate tuple (must have elements)"""
     if 'elements' not in value:
         raise NameError("Tuple must have `elements' property: " + str(value))
     for element_node in value['elements']:
         val_desc(element_node, data_refs_list)
 
-# validate type
+
 def val_desc(value, data_refs_list):
+    """validate type"""
     if value in scalar_types:
         pass
     elif value['type'] in scalar_types:
@@ -136,41 +149,50 @@ def val_desc(value, data_refs_list):
     else:
         raise NameError('Invalid type: ' + str(value))        
 
-# validate PDI data node
+
 def val_data(data_root, data_list, data_refs_list):
+    """validate PDI data node"""
     if data_root:
         for key, value in data_root.items():
             val_desc(value, data_refs_list)
             data_list.append(key)
 
-# validate PDI metadata node
+
 def val_metadata(metadata_root, metadata_list, data_refs_list):
+    """validate PDI metadata node"""
     if metadata_root:
         for key, value in metadata_root.items():
             val_desc(value, data_refs_list)
             metadata_list.append(key)
 
-# check if all references to metadata are correct
+
 def check_data_refs(data_list, metadata_list, data_refs):
+    """check if all references to metadata are correct"""
     for ref in data_refs:
         if ref not in (data_list + metadata_list):
             raise NameError('Reference: `$' + ref + "' is not referencing any data nor metadata")
 
-# validate all plugins
+
 def val_plugins(plugins_root, data_list, metadata_list, data_refs_list):
+    """validate all plugins"""
     if plugins_root:
         for key, plugin in plugins_root.items():
             try:
-                getattr(importlib.import_module("pdicfg_validator.val_"+key+"_config"), "val_"+key)(plugin, data_list, metadata_list, data_refs_list)
+                valkey = getattr(importlib.import_module("pdicfg_validator.val_"+key+"_config"),
+                                 "val_"+key)
+                valkey(plugin, data_list, metadata_list, data_refs_list)
             except NameError as e:
                 raise e
             except ImportError:
                 print("Cannot load " + key + " validation module.")
-                print("Make sure that you have installed " + key + " plugin and $PYTHONPATH is set correctly.")
+                print("Make sure that you have installed " + key + " plugin and "
+                      + "$PYTHONPATH is set correctly.")
                 print("Type 'echo $PYTHONPATH'. It should contain: '$PATH_TO_PDI_PYTHON'")
-                print("If it's not there add it by: export PYTHONPATH=$PATH_TO_PDI_PYTHON/pdicfg_validator:$PYTHONPATH")
+                print("If it's not there add it by: "
+                      + "export PYTHONPATH=$PATH_TO_PDI_PYTHON/pdicfg_validator:$PYTHONPATH")
                 print("Default PATH_TO_PDI_PYTHON is /usr/local/lib/python3/dist-packages")
                 raise
+
 
 def val_plugin_path(plugin_path):
     plugin_path_list = plugin_path.split(os.pathsep)
@@ -193,6 +215,7 @@ def val_plugin_path(plugin_path):
         if not os.path.exists(path):
             raise NameError("'" + path + "' is not valid or existing path")
 
+
 def val_descs_node(types_node, data_refs_list):
     resolved_types_count = 0
     resolved_type = True
@@ -208,37 +231,39 @@ def val_descs_node(types_node, data_refs_list):
                 except BaseException as e:
                     pass
     if resolved_types_count != len(types_node):
-        raise NameError("Cannot define all datatypes from types tree. Loaded successfuly: " + str(scalar_types))
+        raise NameError("Cannot define all datatypes from types tree. Loaded successfuly: "
+                        + str(scalar_types))
+
 
 def run_test(config_file_name):
     # list of declared data
     data_list = []
 
-    #list of declared metadata
+    # list of declared metadata
     metadata_list = []
 
     # metadata that is used (referenced)
     data_refs_list = []
 
     with open(config_file_name, 'r') as config_file:
-        root = yaml.load(config_file)
-        pdi_root = root.get('pdi', root) # if file has pdi subtree use it
+        root = yaml.load(config_file, Loader=Loader)
+        pdi_root = root.get('pdi', root)  # if file has pdi subtree use it
 
         if 'types' in pdi_root:
             val_descs_node(pdi_root.get('types'), data_refs_list)
 
         if 'plugins' in pdi_root:
             val_plugins(pdi_root.get('plugins', False), data_list, metadata_list, data_refs_list)
-        
+
         if 'data' in pdi_root:
             val_data(pdi_root.get('data', False), data_list, data_refs_list)
-        
+
         if 'metadata' in pdi_root:
             val_metadata(pdi_root.get('metadata', False), metadata_list, data_refs_list)
-        
+
         if 'plugin_path' in pdi_root:
             val_plugin_path(pdi_root.get('plugin_path'))
 
         check_data_refs(data_list, metadata_list, data_refs_list)
-        
+
         print("\033[1;32m" + sys.argv[1] + "\033[0;32m is a valid PDI configuration file")
