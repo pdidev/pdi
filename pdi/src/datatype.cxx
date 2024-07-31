dfe134286bd4607a1ea2e1a13df0c964489c393e
Julien Bigot <julien.bigot@cea.fr>
2024-07-31 12:59:23 +0200

    Fixed compilation with {fmt} version 11+
    Fixes #456

--- pdi/src/datatype.cxx
+++ pdi/src/datatype.cxx
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) 2015-2021 Commissariat a l'energie atomique et aux energies alternatives (CEA)
+ * Copyright (C) 2015-2024 Commissariat a l'energie atomique et aux energies alternatives (CEA)
  * Copyright (C) 2021 Institute of Bioorganic Chemistry Polish Academy of Science (PSNC)
  * All rights reserved.
  *
@@ -27,7 +27,8 @@
 
 #include <spdlog/spdlog.h>
 
-#include <pdi/error.h>
+#include "pdi/fmt.h"
+#include "pdi/error.h"
 
 #include "pdi/datatype.h"
 
