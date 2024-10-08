45b6bbf56761f3d73b8202261952b4f96887821f
Julien Bigot <jbigot@users.noreply.github.com>
2024-10-08 16:03:18 -0500

    Update pdi/include/pdi/context.h

--- pdi/include/pdi/context.h
+++ pdi/include/pdi/context.h
@@ -97,8 +97,8 @@ public:
 	virtual Iterator end() = 0;
 
 	/** Find the Data_descriptor corresponding to a given name
-	 * \param[in] name the Data_descriptor name
 	 *
+	 * \param[in] name the Data_descriptor name
 	 * \return an iterator to the requested Data_descriptor name. If no such element is found, past-the-end (see end()) iterator is returned.
 	 */
 	virtual Iterator find(const std::string& name) = 0;
