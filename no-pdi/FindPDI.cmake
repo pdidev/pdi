f53e67638d985b4308858b1acd52395ece24ad2f
Julian Auriac <julian.auriac@cea.fr>
2025-02-20 11:20:56 +0100

    Complete find_package option for no-pdi, update documentation, to do : without paraconf

--- no-pdi/FindPDI.cmake
+++ no-pdi/FindPDI.cmake
@@ -1,16 +1,18 @@
 # Set the default search paths
 set(PDI_DIRS "${CMAKE_CURRENT_LIST_DIR}/..")
 
-# Check if the specific folder exists
-set(NO_PDI_PATH "${CMAKE_MODULE_PATH}")
-
-include(CMakePrintHelpers)
-cmake_print_variables(PDI_DIRS)
-cmake_print_variables(NO_PDI_PATH)
+# Search for a directory in CMAKE_MODULE_PATH that includes "no-pdi",
+# which is passed as an argument to the cmake command
+set(NO_PDI_PATH "")
+foreach(module_path IN LISTS CMAKE_MODULE_PATH)
+  if(module_path MATCHES ".*no-pdi.*")
+    set(NO_PDI_PATH "${module_path}")
+    break()
+  endif()
+endforeach()
 
 if(EXISTS ${NO_PDI_PATH})
     # Set the include directories to the specific folder path
-    message("DEBUG : NO-PDI - NO-PDI")
     set(PDI_INCLUDE_DIRS ${NO_PDI_PATH})
     cmake_print_variables(NO_PDI_PATH)
     set(PDI_LIBRARIES "")
@@ -18,8 +20,6 @@ if(EXISTS ${NO_PDI_PATH})
     include(${NO_PDI_PATH}/no-pdi.cmake)
 else()
     # If the specific folder does not exist, try to find the package using the default search paths
-    message("DEBUG : NO-PDI - PDI")
-    # find_path(PDI_INCLUDE_DIRS NAMES PDIConfig.cmake PATHS ${PDI_DIRS})
     find_path(PDI_INCLUDE_DIRS NAMES PDIConfig.cmake PATHS "<path>/<to>/<pdi>/<root>/pdi/build/staging/share/pdi/cmake/")
     cmake_print_variables(PDI_INCLUDE_DIRS)
     if(PDI_INCLUDE_DIRS)
