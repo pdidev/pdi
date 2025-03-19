0857f73bd0fc34f8b4a5a39fddf00b583c1f67db
Julian Auriac <julian.auriac@cea.fr>
2025-03-19 13:32:52 +0100

    PR feedback fixes 16

--- tests/add_all_tests.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd tests_no-pdi
-rm -rf CMakeCache.txt CMakeFiles/ cmake_install.cmake && cmake .
-cd ../tests_pdi
-rm -rf CMakeCache.txt CMakeFiles/ cmake_install.cmake && cmake .
-cd ..
