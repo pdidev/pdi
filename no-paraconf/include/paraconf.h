ea20ff5c1b54ff36a7da355dec689baa9a9044ff
Julian Auriac <julian.auriac@cea.fr>
2025-07-24 15:00:17 +0200

    Account for Paraconf availability; missing no-paraconf mock for PDI_init 86

--- no-paraconf/include/paraconf.h
+++ no-paraconf/include/paraconf.h
@@ -36,14 +36,18 @@ extern "C" {
  * \{
  */
 
-typedef struct PC_tree_s { } PC_tree_t;
+typedef struct PC_tree_s {
+ } PC_tree_t;
 struct _IO_FILE;
 typedef struct _IO_FILE FILE;
-static inline PC_tree_t PC_parse_string(const char* document) {
+
+static inline PC_tree_t PC_parse_string(const char* document)
+{
 	PC_tree_t mock = {};
 	return mock;
 }
-static inline PC_tree_t PC_parse_file(FILE *file) {
+static inline PC_tree_t PC_parse_file(FILE *file)
+{
 	PC_tree_t mock = {};
 	return mock;
 }
