6a7589b2f800e0808d2ba8221b988698b9b68ea8
Julian Auriac <julian.auriac@cea.fr>
2025-07-24 11:49:29 +0200

    Account for Paraconf availability; missing no-paraconf mock for PDI_init 78

--- no-pdi/include/pdi.h
+++ no-pdi/include/pdi.h
@@ -57,7 +57,7 @@
 
 // #if defined __has_include
 // #if __has_include(<paraconf.h>)
-// #include <paraconf.h>
+#include <paraconf.h>
 // #endif
 // #endif
 
@@ -173,21 +173,27 @@ static inline PDI_errhandler_t PDI_errhandler(PDI_errhandler_t handler)
 // #endif
 // // #endif
 
-#if defined(__has_include)
-  #if __has_include(<paraconf.h>)
-    #warning "INCLUDE"
-    #include <paraconf.h>
-  #else
-    #warning "MOCK"
-    typedef struct PC_tree_s { } PC_tree_t;
-    static inline PC_tree_t PC_parse_string(const char* document) {
-        PC_tree_t mock = {};
-        return mock;
-    }
-  #endif
-#else
-#warning "ELSE"
-#endif
+// #if defined(__has_include)
+//   #if __has_include(<paraconf.h>)
+//     #warning "INCLUDE"
+//     #include <paraconf.h>
+//   #else
+//     #warning "MOCK"
+//     typedef struct PC_tree_s { } PC_tree_t;
+//     struct _IO_FILE;
+//     typedef struct _IO_FILE FILE;
+//     static inline PC_tree_t PC_parse_string(const char* document) {
+//         PC_tree_t mock = {};
+//         return mock;
+//     }
+//     static inline PC_tree_t PC_parse_file(FILE *file) {
+//         PC_tree_t mock = {};
+//         return mock;
+//     }
+//   #endif
+// #else
+// #warning "ELSE"
+// #endif
 
 static inline PDI_status_t PDI_init(PC_tree_t conf)
 {
