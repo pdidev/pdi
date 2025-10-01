b31deef9d87c486d146dd7752cb5121d4dc8ffc8
Julian Auriac <julian.auriac@cea.fr>
2025-10-01 11:32:14 +0200

    Add feedback from 23-09-25 2/2 - Fix the script to run the CI locally

--- run-local-CI.sh
+++ run-local-CI.sh
@@ -31,7 +31,8 @@
 set -euo pipefail
 
 # ----------- Config ------------------
-IMAGE="ghcr.io/pdidev/ubuntu/rolling/openmpi/all:v3"
+# IMAGE="ghcr.io/pdidev/ubuntu/rolling/openmpi/all:v3"
+IMAGE="ghcr.io/pdidev/spack/latest/gcc/mpich/all:v3"
 SCRIPT_NAME="run.sh"
 SCRIPT_DIR="./.ci_tmp"
 REPORT_FILE="tests.xml"
