d14a0ed5e9d460e5b7180aa54274f9153a0afdb8
jacques.morice <jacques.morice@cea.Fr>
2025-12-05 10:23:18 +0100

    change format to snake_case

--- plugins/catalyst/tests/Grid.h
+++ plugins/catalyst/tests/Grid.h
@@ -7,16 +7,16 @@
 class Grid
 {
 public:
-  Grid(const unsigned int numPoints[3], const double spacing[3]);
-  size_t GetNumberOfPoints() const;
-  size_t GetNumberOfCells() const;
-  const double* GetPointsArray() const;
-  const double* GetPoint(size_t pointId) const;
-  const unsigned int* GetCellPoints(size_t cellId) const;
+	Grid(const unsigned int num_points[3], const double spacing[3]);
+	size_t get_number_of_points() const;
+	size_t get_number_of_cells() const;
+	const double* get_points_array() const;
+	const double* get_point(size_t pointId) const;
+	const unsigned int* get_cell_points(size_t cellId) const;
 
 private:
-  std::vector<double> Points;
-  std::vector<unsigned int> Cells;
+	std::vector<double> m_points;
+	std::vector<unsigned int> m_cells;
 };
 
 #endif
