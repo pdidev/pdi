6a2a4da9a0899340e1da68d2dc219f05a2523d45
jacques.morice <jacques.morice@cea.Fr>
2025-12-05 10:23:18 +0100

    change format to snake_case

--- plugins/catalyst/pdi_catalyst_plugin.h
+++ plugins/catalyst/pdi_catalyst_plugin.h
@@ -15,35 +15,38 @@ typedef struct conduit_node_impl conduit_node;
  * from PDI API calls (PDI_init, PDI_multi_expose, PDI_finalize).
  *
  * It leverages the specification tree to copy only pointer to data. The conduit node structure of
- * the catalyst_execute call is defined in the spec tree, and dynamic data are referenced with the
- * special keyword "PDI_data".
+ * the catalyst_execute call is defined in the spec tree.
  *
  */
-class catalyst_plugin : public PDI::Plugin
+class catalyst_plugin: public PDI::Plugin
 {
 public:
-  catalyst_plugin(PDI::Context& ctx, PC_tree_t spec_tree);
-  ~catalyst_plugin();
+	catalyst_plugin(PDI::Context& ctx, PC_tree_t spec_tree);
+	~catalyst_plugin();
 
 private:
-  void ProcessPDIInit();
-  void ProcessData(const std::string& data_name, PDI::Ref ref);
-  void ProcessEvent(const std::string& event_name);
+	void process_PDI_init();
+	void process_data(const std::string& data_name, PDI::Ref ref);
+	void process_event(const std::string& event_name);
 
-  void RunCatalystInitialize();
-  void RunCatalystExecute();
-  void RunCatalystFinalize();
-  void FillNodeWithPDIDataArray(conduit_node* node, PC_tree_t tree);
-  void FillNodeWithScalarPDIData(conduit_node* node, const std::string& name,
-    const PDI::Scalar_datatype& scalar_datatype, PDI::Ref_r& ref_r);
-  void FillNodeWithArrayPDIData(conduit_node* node, const std::string& name, PC_tree_t& tree,
-    const PDI::Array_datatype& array_datatype, PDI::Ref_r& ref_r);
-  long GetLongValueFromSpecNode(PC_tree_t& spec, const std::string& name);
-  std::string ReadPDIExecuteEventName();
+	void run_catalyst_initialize();
+	void run_catalyst_execute();
+	void run_catalyst_finalize();
+	void fill_node_with_PDI_data_array(conduit_node* node, PC_tree_t tree);
+	void fill_node_with_scalar_PDI_data(conduit_node* node, const std::string& name, const PDI::Scalar_datatype& scalar_datatype, PDI::Ref_r& ref_r);
+	void fill_node_with_array_PDI_data(
+		conduit_node* node,
+		const std::string& name,
+		PC_tree_t& tree,
+		const PDI::Array_datatype& array_datatype,
+		PDI::Ref_r& ref_r
+	);
+	long get_long_value_from_spec_node(PC_tree_t& spec, const std::string& name);
+	std::string read_PDI_execute_event_name();
 
-  PC_tree_t SpecTree;
-  std::unordered_map<std::string, PDI::Ref> CurrentPDIData;
-  std::string PDIExecuteEventName;
+	PC_tree_t m_spec_tree;
+	std::unordered_map<std::string, PDI::Ref> m_current_PDI_data;
+	std::string m_PDI_execute_event_name;
 };
 
 PDI_PLUGIN(catalyst)
--- plugins/catalyst/tests/PDIAdaptor.cxx
+++ plugins/catalyst/tests/PDIAdaptor.cxx
@@ -4,85 +4,96 @@
 #include "Grid.h"
 
 #include <iostream>
-#include <pdi.h>
 #include <string>
+#include <pdi.h>
 
-namespace PDIAdaptor
+namespace PDI_adaptor {
+
+bool initialize(const std::string& pdi_yaml_config_file_path, const Grid& grid)
 {
+	PC_tree_t conf = PC_parse_path(pdi_yaml_config_file_path.c_str());
+	auto status = PDI_init(PC_get(conf, ""));
+	if (status != PDI_status_t::PDI_OK) {
+		return false;
+	}
 
-bool Initialize(const std::string& pdi_yaml_config_file_path, const Grid& grid)
-{
-  PC_tree_t conf = PC_parse_path(pdi_yaml_config_file_path.c_str());
-  auto status = PDI_init(PC_get(conf, ""));
-  if (status != PDI_status_t::PDI_OK)
-  {
-    return false;
-  }
+	auto points_array_size = grid.get_number_of_points() * 3;
+	status = PDI_expose("points_array_size", &points_array_size, PDI_OUT);
+	if (status != PDI_status_t::PDI_OK) {
+		return false;
+	}
 
-  auto points_array_size = grid.GetNumberOfPoints() * 3;
-  status = PDI_expose("points_array_size", &points_array_size, PDI_OUT);
-  if (status != PDI_status_t::PDI_OK)
-  {
-    return false;
-  }
+	auto number_of_cells = grid.get_number_of_cells() * 8;
+	status = PDI_expose("cell_points_size", &number_of_cells, PDI_OUT);
+	if (status != PDI_status_t::PDI_OK) {
+		return false;
+	}
 
-  auto number_of_cells = grid.GetNumberOfCells() * 8;
-  status = PDI_expose("cell_points_size", &number_of_cells, PDI_OUT);
-  if (status != PDI_status_t::PDI_OK)
-  {
-    return false;
-  }
+	auto velocity_array_size = grid.get_number_of_points() * 3;
+	status = PDI_expose("velocity_array_size", &velocity_array_size, PDI_OUT);
+	if (status != PDI_status_t::PDI_OK) {
+		return false;
+	}
 
-  auto velocity_array_size = grid.GetNumberOfPoints() * 3;
-  status = PDI_expose("velocity_array_size", &velocity_array_size, PDI_OUT);
-  if (status != PDI_status_t::PDI_OK)
-  {
-    return false;
-  }
+	auto pressure_array_size = grid.get_number_of_cells();
+	status = PDI_expose("pressure_array_size", &pressure_array_size, PDI_OUT);
+	if (status != PDI_status_t::PDI_OK) {
+		return false;
+	}
 
-  auto pressure_array_size = grid.GetNumberOfCells();
-  status = PDI_expose("pressure_array_size", &pressure_array_size, PDI_OUT);
-  if (status != PDI_status_t::PDI_OK)
-  {
-    return false;
-  }
-
-  return true;
+	return true;
 }
 
-bool Execute(int cycle, double time, Grid& grid, Attributes& attribs)
+bool execute(int cycle, double time, Grid& grid, Attributes& attribs)
 {
-  auto number_of_points = grid.GetNumberOfPoints();
-  auto number_of_cells = grid.GetNumberOfCells();
+	auto number_of_points = grid.get_number_of_points();
+	auto number_of_cells = grid.get_number_of_cells();
 
-  auto status = PDI_multi_expose(
-    //
-    "catalyst_execute",
-    //
-    "cycle", &cycle, PDI_OUT,
-    //
-    "time", &time, PDI_OUT,
-    //
-    "number_of_points", &number_of_points, PDI_OUT,
-    //
-    "points_array", grid.GetPointsArray(), PDI_OUT,
-    //
-    "number_of_cells", &number_of_cells, PDI_OUT,
-    //
-    "cell_points", grid.GetCellPoints(0), PDI_OUT,
-    //
-    "velocity_array", attribs.GetVelocityArray(), PDI_OUT,
-    //
-    "pressure_array", attribs.GetPressureArray(), PDI_OUT,
-    //
-    NULL);
+	auto status = PDI_multi_expose(
+		//
+		"catalyst_execute",
+		//
+		"cycle",
+		&cycle,
+		PDI_OUT,
+		//
+		"time",
+		&time,
+		PDI_OUT,
+		//
+		"number_of_points",
+		&number_of_points,
+		PDI_OUT,
+		//
+		"points_array",
+		grid.get_points_array(),
+		PDI_OUT,
+		//
+		"number_of_cells",
+		&number_of_cells,
+		PDI_OUT,
+		//
+		"cell_points",
+		grid.get_cell_points(0),
+		PDI_OUT,
+		//
+		"velocity_array",
+		attribs.get_velocity_array(),
+		PDI_OUT,
+		//
+		"pressure_array",
+		attribs.get_pressure_array(),
+		PDI_OUT,
+		//
+		NULL
+	);
 
-  return status == PDI_status_t::PDI_OK;
+	return status == PDI_status_t::PDI_OK;
 }
 
-bool Finalize()
+bool finalize()
 {
-  auto status = PDI_finalize();
-  return status == PDI_status_t::PDI_OK;
+	auto status = PDI_finalize();
+	return status == PDI_status_t::PDI_OK;
 }
-}
+} // namespace PDI_adaptor
