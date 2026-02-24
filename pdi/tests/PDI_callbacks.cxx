4805e94f568e63458aa6a62d11c1ad6e5c835105
jacques.morice <jacques.morice@cea.Fr>
2026-02-24 09:34:26 +0100

    update copyright

--- pdi/tests/PDI_callbacks.cxx
+++ pdi/tests/PDI_callbacks.cxx
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) 2020-2021 Commissariat a l'energie atomique et aux energies alternatives (CEA)
+ * Copyright (C) 2020-2026 Commissariat a l'energie atomique et aux energies alternatives (CEA)
  * Copyright (C) 2018 Institute of Bioorganic Chemistry Polish Academy of Science (PSNC)
  * All rights reserved.
  *
@@ -23,7 +23,6 @@
  * THE SOFTWARE.
  ******************************************************************************/
 
-#include <iostream>
 #include <gtest/gtest.h>
 
 #include <pdi/context.h>
