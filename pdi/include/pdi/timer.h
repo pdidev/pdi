d0f35402a6e138b4e1f2e1d23cde0f00fddaf6ed
yushan wang <yushan.wang@cea.fr>
2026-03-12 15:42:02 +0100

    add a timer class

---
+++ pdi/include/pdi/timer.h
@@ -0,0 +1,76 @@
+/*******************************************************************************
+ * Copyright (C) 2015-2019 Commissariat a l'energie atomique et aux energies alternatives (CEA)
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ * * Neither the name of CEA nor the names of its contributors may be used to
+ *   endorse or promote products derived from this software without specific
+ *   prior written permission.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ ******************************************************************************/
+
+#ifndef PDI_TIMER_H_
+#define PDI_TIMER_H_
+
+#include <iostream>
+// #include <vector>
+#include <string>
+#include <map>
+#include <chrono>
+// #include <string>
+#include <utility>
+
+// #include <spdlog/logger.h>
+// #include <spdlog/spdlog.h>
+
+#include <pdi/pdi_fwd.h>
+// #include <pdi/paraconf_wrapper.h>
+namespace PDI {
+
+class PDI_EXPORT TimerManager {
+public:
+    // Delete copy constructor and assignment
+    TimerManager(const TimerManager&) = delete;
+    void operator=(const TimerManager&) = delete;
+
+    static TimerManager& getInstance();
+
+    void startTimer(const std::string& name);
+    void stopTimer(const std::string& name);
+    void printReport() const ;
+    void printReport(const std::string& name) const;
+
+    const std::map<std::string, double>& getResults();
+
+private:
+    TimerManager() {} // Private Constructor
+
+    std::map<std::string, std::chrono::high_resolution_clock::time_point> start_times;
+    std::map<std::string, double> accumulated_times;
+};
+}
+#ifdef WITH_TIMER_REPORT
+    #define START_TIMER(name) PDI::TimerManager::getInstance().startTimer(name)
+    #define STOP_TIMER(name)  PDI::TimerManager::getInstance().stopTimer(name)
+    #define PRINT_TIMER_REPORT() PDI::TimerManager::getInstance().printReport()
+#else
+    #define START_TIMER(name)
+    #define STOP_TIMER(name)
+    #define PRINT_TIMER_REPORT()
+#endif
+
+
+#endif // PDI_TIMER_H_
---
+++ pdi/src/timer.cxx
@@ -0,0 +1,72 @@
+/*******************************************************************************
+ * Copyright (C) 2015-2019 Commissariat a l'energie atomique et aux energies alternatives (CEA)
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ * * Neither the name of CEA nor the names of its contributors may be used to
+ *   endorse or promote products derived from this software without specific
+ *   prior written permission.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ ******************************************************************************/
+
+#include <pdi/timer.h>
+namespace PDI {
+    
+
+    TimerManager& TimerManager::getInstance() {
+        static TimerManager instance;
+        return instance;
+    }
+
+    // Start a timer by name
+    void TimerManager::startTimer(const std::string& name) {
+        start_times[name] = std::chrono::high_resolution_clock::now();
+    }
+
+    // Stop a timer and accumulate the duration
+    void TimerManager::stopTimer(const std::string& name) {
+        auto end_time = std::chrono::high_resolution_clock::now();
+        
+        if (start_times.find(name) != start_times.end()) {
+            std::chrono::duration<double> elapsed = end_time - start_times[name];
+            accumulated_times[name] += elapsed.count();
+        }
+    }
+
+    // Export the results
+    void TimerManager::printReport() const {
+        std::cout << "\n--- Full Timer Report ---"<<&accumulated_times<<std::endl;
+        for (const auto& [name, duration] : accumulated_times) {
+            std::cout << name << ": " << duration << " seconds" << std::endl;
+        }
+    }
+
+    void TimerManager::printReport(const std::string& name) const {
+        std::cout << "\n--- Timer Report : "<<&accumulated_times<<std::endl;
+        auto it = accumulated_times.find(name);
+    
+        if (it != accumulated_times.end()) {
+            std::cout << name << " : " << it->second << " seconds" << std::endl;
+        } else {
+            std::cout << name << " timer not found." << std::endl;
+        }
+    }
+
+    // Get the map directly (useful for MPI export)
+    const std::map<std::string, double>& TimerManager::getResults() {
+        return accumulated_times;
+    }
+}
