aec7cd3137b2c5568c2993fc46a7a6f03c4d980b
Iole Bolognesi <bolognesi.iole@gmail.com>
2026-04-20 12:27:12 +0200

    added copyright headers

--- plugins/VeloC/veloc_cfg.cxx
+++ plugins/VeloC/veloc_cfg.cxx
@@ -21,7 +21,6 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  ******************************************************************************/
-
 #include <pdi/context.h>
 #include <pdi/paraconf_wrapper.h>
 #include <veloc.h>
