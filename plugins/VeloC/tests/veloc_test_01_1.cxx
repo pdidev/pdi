d3f1088ede1e07c7f74372fa4d4087c242df7c5b
Iole Bolognesi <bolognesi.iole@gmail.com>
2026-04-20 17:28:18 +0200

    include mpi header that was deleted by mistake

--- plugins/VeloC/tests/veloc_test_01_1.cxx
+++ plugins/VeloC/tests/veloc_test_01_1.cxx
@@ -22,7 +22,8 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  ******************************************************************************/
-
+#include <mpi.h>
+#include <iostream>
 #include <pdi.h>
 
 const char CONF_YAML[]
