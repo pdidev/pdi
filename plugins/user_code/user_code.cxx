862da50822eb9d7d40c2e9c23f3bf5bf2f954cce
yushan wang <yushan.wang@cea.fr>
2026-06-16 14:33:15 +0200

    Remove timer

--- plugins/user_code/user_code.cxx
+++ plugins/user_code/user_code.cxx
@@ -164,7 +164,6 @@ public:
 	/// call the function that has been registered
 	void call(Context& ctx)
 	{
-		ctx.event("user_code_start_timer");
 		// all exposed aliases that will be unexposed on destroy
 		vector<ExposedAlias> exposed_aliases;
 		for (auto&& alias: m_aliases) {
@@ -180,7 +179,6 @@ public:
 		} catch (...) {
 			ctx.logger().error("While calling user code, caught exception");
 		}
-		ctx.event("user_code_stop_timer");
 	}
 
 }; // class Trigger
