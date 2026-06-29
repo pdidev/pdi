54f1ee7a0b10262bcce76afc34c6c9cd2e6e8ba0
jacques.morice <jacques.morice@cea.Fr>
2026-06-29 12:03:17 +0200

    adding runtest_dir for other tests

--- plugins/catalyst/tests/run_test_mpi.py
+++ plugins/catalyst/tests/run_test_mpi.py
@@ -12,8 +12,10 @@ binary_folder = sys.argv[1]
 source_folder = sys.argv[2]
 mpi_exec = sys.argv[3]
 
+pwd_value = os.getcwd() # get the current directory
+
 env = os.environ.copy()
-env["CATALYST_DATA_DUMP_DIRECTORY"] = binary_folder
+env["CATALYST_DATA_DUMP_DIRECTORY"] = pwd_value
 env["CATALYST_IMPLEMENTATION_NAME"] = 'stub' # need to get the conduit json file for comparison
 env["PDI_PLUGIN_PATH"] = binary_folder + '/..'
 result = subprocess.run([mpi_exec, "-np", "4", binary_folder + "/TestPDICatalyst", binary_folder + "/pdi.yml"], env=env)
@@ -31,7 +33,7 @@ if(endianness == 'big'):
 # Check the initialize json dump for each rank.
 reference_initialize_json = source_folder + "/" + reference_directory + "/initialize_reference.json"
 for rank in range(4):
-    actual_initialize_json = binary_folder + f"initialize_params.conduit_bin.4.{rank}_json"
+    actual_initialize_json = pwd_value + "/" + f"initialize_params.conduit_bin.4.{rank}_json"
     with open(reference_initialize_json) as ref_file:
         with open(actual_initialize_json) as actual_file:
             ref_json = json.load(ref_file)
@@ -47,7 +49,7 @@ for rank in range(4):
 for rank in range(4):
     reference_execute_json = source_folder + "/" + reference_directory + f"/execute_reference_rank{rank}.json"
     for step in range(9):
-        filepath = binary_folder + f"execute_invc{step}_params.conduit_bin.4.{rank}_json"
+        filepath = pwd_value + "/" + f"execute_invc{step}_params.conduit_bin.4.{rank}_json"
         if not filecmp.cmp(reference_execute_json, filepath):
             print(f'Differences detected in file "{filepath}" compared to reference "{reference_execute_json}')
             exit(1)
@@ -55,7 +57,7 @@ for rank in range(4):
 # Check the finalize json dump for each rank.
 reference_finalize_json = source_folder + "/" + reference_directory + "/finalize_reference.json"
 for rank in range(4):
-    actual_finalize_json = binary_folder + f"finalize_params.conduit_bin.4.{rank}_json"
+    actual_finalize_json = pwd_value + "/" + f"finalize_params.conduit_bin.4.{rank}_json"
     if not filecmp.cmp(reference_finalize_json, actual_finalize_json):
         print(f'Differences detected in file "{actual_finalize_json}" compared to reference "{reference_finalize_json}')
         exit(1)
