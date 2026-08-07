90df486b9fc971f63533a7dc6106c41d9fa1a9d5
Iole Bolognesi <bolognesi.iole@gmail.com>
2026-08-07 15:32:14 +0200

    remove double negative from PDI example according to review comments

--- example/example.c
+++ example/example.c
@@ -26,6 +26,7 @@
 #include <assert.h>
 #include <math.h>
 #ifndef WITHOUT_PARACONF
+#define WITH_PARACONF
 #include <paraconf.h>
 #endif
 #include <stdio.h>
@@ -160,14 +161,12 @@ int main(int argc, char* argv[])
 		exit(1);
 	}
 
-#ifndef WITHOUT_PARACONF
+#ifdef WITH_PARACONF
 	PC_tree_t conf = PC_parse_path(argv[1]);
+	PDI_init(PC_get(conf, ".pdi"));
 #endif
 
 	MPI_Comm main_comm = MPI_COMM_WORLD;
-#ifndef WITHOUT_PARACONF
-	PDI_init(PC_get(conf, ".pdi"));
-#endif
 
 	PDI_expose("mpi_comm", &main_comm, PDI_INOUT);
 
@@ -183,38 +182,34 @@ int main(int argc, char* argv[])
 	long longval;
 
 	int dsize[2];
-#ifndef WITHOUT_PARACONF
+
+#ifdef WITH_PARACONF
 	PC_int(PC_get(conf, ".datasize[0]"), &longval);
-#else
-	longval = 8; // size is 8 by default because, why not
-#endif
 	dsize[0] = longval;
-#ifndef WITHOUT_PARACONF
+
 	PC_int(PC_get(conf, ".datasize[1]"), &longval);
-#else
-	longval = 8 * psize_1d; // default size is 8 * psize_1d so that it's easy to parallelize over psize_1d processes
-#endif
 	dsize[1] = longval;
 
 	int psize[2];
-#ifndef WITHOUT_PARACONF
+
 	PC_int(PC_get(conf, ".parallelism.height"), &longval);
-#else
-	longval = 1;
-#endif
 	psize[0] = longval;
-#ifndef WITHOUT_PARACONF
+
 	PC_int(PC_get(conf, ".parallelism.width"), &longval);
-#else
-	longval = psize_1d; // all the parallelism is here by default
-#endif
 	psize[1] = longval;
 
 	double duration;
-#ifndef WITHOUT_PARACONF
 	PC_double(PC_get(conf, ".duration"), &duration);
+
 #else
-	duration = 0.1;
+	dsize[0] = 8;
+	dsize[1] = 8 * psize_1d;
+
+	int psize[2];
+	psize[0] = 1;
+	psize[1] = psize_1d;
+
+	double duration = 0.1;
 #endif
 
 	// get local & add ghosts to sizes
@@ -246,7 +241,7 @@ int main(int argc, char* argv[])
 	long cp_status;
 	int first_iter=1;
 
-#ifndef WITHOUT_PARACONF
+#ifdef WITH_PARACONF
 	PC_errhandler_t old_handler = PC_errhandler(PC_NULL_HANDLER); 
 	PC_tree_t status_tree = PC_get(conf, ".cp_status");
 	PC_errhandler(old_handler);  
@@ -303,7 +298,7 @@ int main(int argc, char* argv[])
 
 	PDI_finalize();
 
-#ifndef WITHOUT_PARACONF
+#ifdef WITH_PARACONF
 	PC_tree_destroy(&conf);
 #endif
 
