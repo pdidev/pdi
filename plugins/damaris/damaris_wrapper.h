4559edbe46cbab3669e622b00a74c8c287497f37
Etienne Ndamlabin <ndamlabin@gmail.com>
2026-09-03 15:45:53 +0200

    Update copyright headers to 2025-2026 (CEA) / 2024-2026 (Inria)

--- plugins/damaris/damaris_wrapper.h
+++ plugins/damaris/damaris_wrapper.h
@@ -1,6 +1,6 @@
 /*******************************************************************************
- * Copyright (C) 2015-2024 Commissariat a l'energie atomique et aux energies alternatives (CEA)
- * Copyright (C) 2024-2025 National Institute for Research in Digital Science and Technology (Inria)
+ * Copyright (C) 2025-2026 Commissariat a l'energie atomique et aux energies alternatives (CEA)
+ * Copyright (C) 2024-2026 National Institute for Research in Digital Science and Technology (Inria)
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
